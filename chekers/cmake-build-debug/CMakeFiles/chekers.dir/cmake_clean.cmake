e550ebb5ef5bdaffc22da3f915937a02fd36c812
vladbogini01-huel <vladbogini01@gmail.com>
2026-02-23 13:37:30 +0300

    test

---
+++ chekers/cmake-build-debug/CMakeFiles/chekers.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chekers.dir/Board/board.cpp.o"
+  "CMakeFiles/chekers.dir/Board/board.cpp.o.d"
+  "CMakeFiles/chekers.dir/main.cpp.o"
+  "CMakeFiles/chekers.dir/main.cpp.o.d"
+  "chekers"
+  "chekers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chekers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
